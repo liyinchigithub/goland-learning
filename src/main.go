393ea8b3b5cfe432e0d077f3c207f78edeb77d7b
李银池 <liyinchi@qq.com>
2022-02-14 23:20:58 +0800

    重命名包

--- src/main.go
+++ src/main.go
@@ -15,7 +15,13 @@ func main() {
 	// 数据库操作
 
 	// os
-	test_go_os()
+	var arr = os.Args
+	fmt.Println(arr)
+	if arr[0] == "ok" {
+		os.Exit(0) //main方法退出
+	} else {
+		println(`不是OK%v`, arr[1])
+	}
 }
 
 /*
